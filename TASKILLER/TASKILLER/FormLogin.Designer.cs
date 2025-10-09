7980e1d2dd8cab6a75a0f651afc7492b62682fa3
sgomezl <sgomezl2425@politecnics.barcelona>
2025-10-09 20:14:22 +0200

    Añado form login

--- TASKILLER/TASKILLER/FormLogin.Designer.cs
+++ TASKILLER/TASKILLER/FormLogin.Designer.cs
@@ -128,6 +128,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormLogin";
             this.Text = "TASKILLER - Log-in";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
