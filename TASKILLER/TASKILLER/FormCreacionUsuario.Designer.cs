68c8871cd6bcead4046601a7a40cfe99dded1f5f
sgomezl <sgomezl2425@politecnics.barcelona>
2025-11-10 20:13:07 +0100

    elimino barra inicio donde no toca

--- TASKILLER/TASKILLER/FormCreacionUsuario.Designer.cs
+++ TASKILLER/TASKILLER/FormCreacionUsuario.Designer.cs
@@ -46,21 +46,8 @@
             this.labelRolUsuario = new System.Windows.Forms.Label();
             this.labelAdvertenciaCreacionUsuarios = new System.Windows.Forms.Label();
             this.panelCreacionUser = new System.Windows.Forms.Panel();
-            this.toolStripInicio = new System.Windows.Forms.ToolStrip();
-            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
-            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.proyectosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.crearNuevoProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.modificarEliminarProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.crearNuevoUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rolesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnUsuario = new System.Windows.Forms.ToolStripButton();
-            this.btnTaskiller = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLOGO)).BeginInit();
             this.panelCreacionUser.SuspendLayout();
-            this.toolStripInicio.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxRolUsuario
@@ -218,7 +205,6 @@
             // panelCreacionUser
             // 
             this.panelCreacionUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            this.panelCreacionUser.Controls.Add(this.toolStripInicio);
             this.panelCreacionUser.Controls.Add(this.textBoxNombreUsuario);
             this.panelCreacionUser.Controls.Add(this.labelAdvertenciaCreacionUsuarios);
             this.panelCreacionUser.Controls.Add(this.pictureBoxLOGO);
@@ -242,112 +228,6 @@
             this.panelCreacionUser.TabIndex = 16;
             this.panelCreacionUser.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCreacionUser_Paint);
             // 
-            // toolStripInicio
-            // 
-            this.toolStripInicio.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStripInicio.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStripInicio.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripDropDownButton1,
-            this.btnUsuario,
-            this.btnTaskiller});
-            this.toolStripInicio.Location = new System.Drawing.Point(0, 0);
-            this.toolStripInicio.Name = "toolStripInicio";
-            this.toolStripInicio.Size = new System.Drawing.Size(2396, 27);
-            this.toolStripInicio.TabIndex = 16;
-            this.toolStripInicio.Text = "toolStrip1";
-            // 
-            // toolStripDropDownButton1
-            // 
-            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inicioToolStripMenuItem,
-            this.proyectosToolStripMenuItem,
-            this.usuariosToolStripMenuItem,
-            this.rolesToolStripMenuItem,
-            this.salirToolStripMenuItem});
-            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(34, 24);
-            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
-            // 
-            // inicioToolStripMenuItem
-            // 
-            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.inicioToolStripMenuItem.Text = "Inicio";
-            // 
-            // proyectosToolStripMenuItem
-            // 
-            this.proyectosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.crearNuevoProyectoToolStripMenuItem,
-            this.modificarEliminarProyectoToolStripMenuItem});
-            this.proyectosToolStripMenuItem.Name = "proyectosToolStripMenuItem";
-            this.proyectosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.proyectosToolStripMenuItem.Text = "Proyectos";
-            // 
-            // crearNuevoProyectoToolStripMenuItem
-            // 
-            this.crearNuevoProyectoToolStripMenuItem.Name = "crearNuevoProyectoToolStripMenuItem";
-            this.crearNuevoProyectoToolStripMenuItem.Size = new System.Drawing.Size(279, 26);
-            this.crearNuevoProyectoToolStripMenuItem.Text = "Crear nuevo proyecto";
-            this.crearNuevoProyectoToolStripMenuItem.Click += new System.EventHandler(this.crearNuevoProyectoToolStripMenuItem_Click);
-            // 
-            // modificarEliminarProyectoToolStripMenuItem
-            // 
-            this.modificarEliminarProyectoToolStripMenuItem.Name = "modificarEliminarProyectoToolStripMenuItem";
-            this.modificarEliminarProyectoToolStripMenuItem.Size = new System.Drawing.Size(279, 26);
-            this.modificarEliminarProyectoToolStripMenuItem.Text = "Modificar/Eliminar proyecto";
-            this.modificarEliminarProyectoToolStripMenuItem.Click += new System.EventHandler(this.modificarEliminarProyectoToolStripMenuItem_Click);
-            // 
-            // usuariosToolStripMenuItem
-            // 
-            this.usuariosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.crearNuevoUsuarioToolStripMenuItem});
-            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
-            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.usuariosToolStripMenuItem.Text = "Usuarios";
-            // 
-            // crearNuevoUsuarioToolStripMenuItem
-            // 
-            this.crearNuevoUsuarioToolStripMenuItem.Name = "crearNuevoUsuarioToolStripMenuItem";
-            this.crearNuevoUsuarioToolStripMenuItem.Size = new System.Drawing.Size(223, 26);
-            this.crearNuevoUsuarioToolStripMenuItem.Text = "Crear nuevo usuario";
-            this.crearNuevoUsuarioToolStripMenuItem.Click += new System.EventHandler(this.crearNuevoUsuarioToolStripMenuItem_Click);
-            // 
-            // rolesToolStripMenuItem
-            // 
-            this.rolesToolStripMenuItem.Name = "rolesToolStripMenuItem";
-            this.rolesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.rolesToolStripMenuItem.Text = "Roles";
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("salirToolStripMenuItem.Image")));
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.salirToolStripMenuItem.Text = "Salir";
-            // 
-            // btnUsuario
-            // 
-            this.btnUsuario.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.btnUsuario.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnUsuario.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuario.Image")));
-            this.btnUsuario.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnUsuario.Name = "btnUsuario";
-            this.btnUsuario.Size = new System.Drawing.Size(29, 24);
-            this.btnUsuario.Text = "Usuario";
-            // 
-            // btnTaskiller
-            // 
-            this.btnTaskiller.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnTaskiller.Image = ((System.Drawing.Image)(resources.GetObject("btnTaskiller.Image")));
-            this.btnTaskiller.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnTaskiller.Name = "btnTaskiller";
-            this.btnTaskiller.Size = new System.Drawing.Size(29, 24);
-            this.btnTaskiller.Text = "TaskillerLogo";
-            // 
             // FormCreacionUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -364,8 +244,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLOGO)).EndInit();
             this.panelCreacionUser.ResumeLayout(false);
             this.panelCreacionUser.PerformLayout();
-            this.toolStripInicio.ResumeLayout(false);
-            this.toolStripInicio.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -389,17 +267,5 @@
         private System.Windows.Forms.Label labelRolUsuario;
         private System.Windows.Forms.Label labelAdvertenciaCreacionUsuarios;
         private System.Windows.Forms.Panel panelCreacionUser;
-        private System.Windows.Forms.ToolStrip toolStripInicio;
-        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
-        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem proyectosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem crearNuevoProyectoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem modificarEliminarProyectoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem crearNuevoUsuarioToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem rolesToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
-        private System.Windows.Forms.ToolStripButton btnUsuario;
-        private System.Windows.Forms.ToolStripButton btnTaskiller;
     }
 }
