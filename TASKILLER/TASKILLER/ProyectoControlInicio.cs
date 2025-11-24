d81d9687ff93dbfc838037a8b4ca20b9142c3af2
sgomezl <sgomezl2425@politecnics.barcelona>
2025-11-24 19:33:00 +0100

    inicio creación vista proyectos pantalla inicio

---
+++ TASKILLER/TASKILLER/ProyectoControlInicio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TASKILLER
+{
+    public partial class ProyectoControlInicio : UserControl
+    {
+        public ProyectoControlInicio()
+        {
+            InitializeComponent();
+        }
+
+        public void ContarTareas(List<Tarea> tareas, Proyecto p)
+        {
+            int contador = 0;
+            flowLayoutPanelNumeroTareas.Controls.Clear();
+
+            foreach (var t in tareas)
+            {
+                contador++;
+            }
+        }
+    }
+}
