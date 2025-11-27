abaaa114b4a96dd45c12d950ea20cf5eff2a28ad
Mireia Nòbrega <nobregaguerram@gmail.com>
2025-11-27 23:42:26 +0100

    programacion form edicion usuarios acabada

--- TASKILLER/TASKILLER/FormEdicionUsuario.cs
+++ TASKILLER/TASKILLER/FormEdicionUsuario.cs
@@ -18,11 +18,16 @@ namespace TASKILLER
         public FormEdicionUsuario(Datos datos, Usuario usuario)
         {
             InitializeComponent();
-            this.Resize += FormEdicion_Resize;
             this.d = datos;
             this.u = usuario;
-            CargarLayout();
 
+            this.Resize += FormEdicion_Resize;
+
+            CargarLayout();
+            CargarRoles();
+            CargarDatosUsuario();
+
+            buttonEdicionUsuario.Click += buttonEdicionUsuario_Click;
         }
 
         private void CargarLayout()
@@ -51,6 +56,73 @@ namespace TASKILLER
             panelEdicionUser.Top = (this.ClientSize.Height - panelEdicionUser.Height) / 2;
         }
 
+        private void CargarRoles()
+        {
+            comboBoxRolEdicionUsuario.DataSource = d.listaRoles;
+            comboBoxRolEdicionUsuario.DisplayMember = "Nombre";
+            comboBoxRolEdicionUsuario.ValueMember = "Id";
+        }
+
+        private void CargarDatosUsuario()
+        {
+            textBoxNombreEdicionUsuario.Text = u.Nombre;
+            textBoxApellidoEdicionUsuario.Text = u.Apellido;
+            textBoxCorreoEdicionUsuario.Text = u.Mail;
+            textBoxContrasenaEdicionUsuario.Text = u.Contrasena;
+            textBoxRepContrasenaEdicionUsuario.Text = u.Contrasena;
+            
+            //rol q ya te el user
+            if (u.Rol != Guid.Empty)
+            {
+                comboBoxRolEdicionUsuario.SelectedValue = u.Rol;
+            }
+        }
+
+        private void buttonEdicionUsuario_Click(object sender, EventArgs e)
+        {
+            //validar no camps buits
+            if (string.IsNullOrWhiteSpace(textBoxNombreEdicionUsuario.Text) ||
+                string.IsNullOrWhiteSpace(textBoxApellidoEdicionUsuario.Text) ||
+                string.IsNullOrWhiteSpace(textBoxCorreoEdicionUsuario.Text) ||
+                string.IsNullOrWhiteSpace(textBoxContrasenaEdicionUsuario.Text))
+            {
+                labelAdvertenciaEdicionUsuarios.Text = "No puedes dejar campos vacíos.";
+            }
+
+            //validar contrñ coinc
+            if (textBoxContrasenaEdicionUsuario.Text != textBoxRepContrasenaEdicionUsuario.Text)
+            {
+                labelAdvertenciaEdicionUsuarios.Text = "Las contraseñas no coinciden.";
+            }
+
+            //save canvi
+            try
+            {
+                //override dades usr
+                u.Nombre = textBoxNombreEdicionUsuario.Text;
+                u.Apellido = textBoxApellidoEdicionUsuario.Text;
+                u.Mail = textBoxCorreoEdicionUsuario.Text;
+                u.Contrasena = textBoxContrasenaEdicionUsuario.Text;
+
+                //actu rol
+                if (comboBoxRolEdicionUsuario.SelectedValue != null)
+                {
+                    u.Rol = (Guid)comboBoxRolEdicionUsuario.SelectedValue;
+                }
+                
+                //misj yeyyy
+                MessageBox.Show("Usuario actualizado correctamente.");
+                FormListaUsuarios f = new FormListaUsuarios(d, u);
+                f.Show();
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                //msj nope yey
+                MessageBox.Show("Error al guardar cambios: " + ex.Message);
+            }
+        }
 
 
     }
